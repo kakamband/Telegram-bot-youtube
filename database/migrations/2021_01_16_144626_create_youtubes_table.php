8a435ee51c3d87dbc5d549db13b271c5f495a7ee
Qvei <46649793+Qvei@users.noreply.github.com>
2021-01-16 15:50:59 +0200

    Add files via upload

---
+++ database/migrations/2021_01_16_144626_create_youtubes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateYoutubesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('youtubes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('channelTitle');
+            $table->string('videoId');
+            $table->string('channelId');
+            $table->string('publishedAt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('youtubes');
+    }
+}
---
+++ database/migrations/2021_01_16_145251_create_inboxes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInboxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inboxes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('upd_from_id');
+            $table->string('keyword');
+            $table->string('key1');
+            $table->string('key2');
+            $table->integer('message_num');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inboxes');
+    }
+}
